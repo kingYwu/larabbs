b3636efcb113dd37b3be7cade988a79feb2787b8
vagrant <vagrant@homestead>
2019-03-22 16:40:01 +0000

    邮箱认证

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@
 
 Route::get('/','PagesController@root')->name('root');
 
-Auth::routes();
+/*Auth::routes();*/
 
-/*
- * // 用户身份验证相关的路由
+
+// 用户身份验证相关的路由
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
@@ -35,5 +35,5 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
- * */
+
 
