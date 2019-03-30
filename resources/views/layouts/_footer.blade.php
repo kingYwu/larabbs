fda8bf5813f5a27f468e6fdc3c38551822a5b7f1
vagrant <vagrant@homestead>
2019-03-30 15:51:14 +0000

    用户可以创建话题

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="footer">
   <div class="container">
     <p class="float-left">
-      由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">kingY</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+      由 <a href="https://weibo.com/u/5661832257?is_hot=1" target="_blank">kingY</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
 
-    <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+    <p class="float-right"><a href="mailto:1647270803@qq.com">联系我们</a></p>
   </div>
 </footer>
