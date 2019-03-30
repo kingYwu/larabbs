98f79a55c2ae12f04c446cbe24fb73cb3a5e4979
vagrant <vagrant@homestead>
2019-03-30 03:56:24 +0000

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,5 @@ Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
  * */
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories','CategoriesController',['only'=>['show']]);
