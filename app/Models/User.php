cccd73e1a177233a22f161ad643ef566fbe08c56
vagrant <vagrant@homestead>
2019-04-01 14:01:22 +0000

    用户可以删除自己发的话题

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,6 +26,11 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Topic::class);
     }
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
 
 
