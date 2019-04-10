c69e2b8a79020d93d51e160156a72efa6ee8c7fc
vagrant <vagrant@homestead>
2019-04-10 02:22:33 +0000

    评论消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,16 +3,20 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class ReplyObserver
 {
-    public  function  created(Reply $reply)
+    public function created(Reply $reply)
     {
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
     public function creating(Reply $reply)
     {
