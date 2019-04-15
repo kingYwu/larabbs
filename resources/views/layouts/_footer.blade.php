a4b4676c61f84edd761f6916e16fb818ceae5de2
vagrant <vagrant@homestead>
2019-04-15 02:51:17 +0000

    站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
       由 <a href="https://weibo.com/u/5661832257?is_hot=1" target="_blank">kingY</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
 
-    <p class="float-right"><a href="mailto:1647270803@qq.com">联系我们</a></p>
+    <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
   </div>
 </footer>
