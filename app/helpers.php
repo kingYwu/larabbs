1448b0a18cf343c6f5e67b88b25efc3b50419fb1
vagrant <vagrant@homestead>
2019-04-15 01:00:01 +0000

    内容管理后台

--- app/helpers.php
+++ app/helpers.php
@@ -13,3 +13,24 @@ function make_excerpt($value,$length = 200)
     $excerpt = trim(preg_replace('/\r\n|\r|\n+/','',strip_tags($value)));
     return str_limit($excerpt,$length);
 }
+function model_admin_link($title,$model)
+{
+    return model_link($title,$model,'admin');
+}
+function model_link($title,$model,$prefix = '')
+{
+    $model_name=model_plural_name($model);
+
+    $prefix=$prefix?"/$prefix/":'/';
+
+    $url = config('app.url').$prefix.$model_name.'/'.$model->id;
+
+    return '<a href="' . $url . '" target="_blank">' . $title . '</a>';
+}
+function model_plural_name($model)
+{
+    $full_class_name=get_class($model);
+    $class_name=class_basename($full_class_name);
+    $snake_case_name=snake_case($class_name);
+    return str_plural($snake_case_name);
+}
